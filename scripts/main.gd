e1be52f77a429f5c9ebe271a95e2789bb6dd6494
Matt Bowman <mab181@duke.edu>
2025-01-22 20:03:15 -0500

    Add day timer to main and load in currentRound to first round

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 
+var currentRound = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	currentRound = load("res://scenes/RoundOne.tscn")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_day_timer_timeout() -> void:
+	#currentRound.processDay()
+	$DayTimer.start()
+	pass # Replace with function body.
