d1c9248b6df2576a502544ac0a189cf3e89b8e4b
Matt Bowman <mab181@duke.edu>
2025-01-22 20:58:41 -0500

    game play  loop terminates now after fixed num days

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,7 @@ var currentRound = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	currentRound = load("res://scenes/roundOne.tscn").instantiate()
+	currentRound.roundOver.connect(endRound)
 	add_child(currentRound)
 	$DayTimer.start()
 	pass # Replace with function body.
@@ -19,3 +20,7 @@ func _on_day_timer_timeout() -> void:
 	print("Bing! Next day")
 	currentRound.processDay()
 	$DayTimer.start()
+
+func endRound() -> void:
+	print("Round is over! Uh oh!")
+	$DayTimer.stop()
--- scripts/round.gd
+++ scripts/round.gd
@@ -1,15 +1,22 @@
 extends Node
 
+signal roundOver
+var day
+@export var maxDays = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	day = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func processDay() -> void:
+	day += 1
+	print("Day in round: " + str(day))
+	if(day > maxDays):
+		roundOver.emit()
+		return
 	var orders: Array[Order] = []
 	$SineMarket.updatePrice(orders)
 	print($SineMarket.getCurrentPrice())
