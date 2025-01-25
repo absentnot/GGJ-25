aeefc5e34e8c8d779d659a2a0183dc58ae87c69e
newmsd <30535987+newmsd@users.noreply.github.com>
2025-01-24 20:08:02 -0500

    Title screen
    Title screen

---
+++ titlescreen/title_screen.gd
@@ -0,0 +1,37 @@
+extends Control
+@onready var background_music = $AudioStreamPlayer
+@onready var hslider=$VolumeSlider
+var master_bus=AudioServer.get_bus_index("Master")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	var music_stream = load("res://start + regular game loop.mp3")
+	music_stream.loop = true
+	background_music.stream = music_stream
+	background_music.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	hslider.visible=!hslider.visible
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus,true)
+	else:
+		AudioServer.set_bus_mute(master_bus,false)
+		
