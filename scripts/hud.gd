a06d39ee83478e000754c743044ce621dea6be0f
Matt Bowman <mab181@duke.edu>
2025-01-25 15:39:14 -0500

    Player order visualizations working.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,6 +6,11 @@ var orderQuantity: int = 0
 var currentShares: int = 0
 var currentMoney: int = 100
 var marketVal: int = 0
+@onready var graph = get_node("MarketVbox/Graph")
+@onready var marketValueDisplay = get_node("MarketVbox/MarketValueDisplay")
+@onready var playerCashDisplay = get_node("PlayerStats/PlayerCashDisplay")
+@onready var playerSharesDisplay = get_node("PlayerStats/PlayerSharesDisplay")
+@onready var orderVisualizer = get_node("PlayerActionsHBox/OrderVisualizer")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_updateCashAndShares(Order.new(0))
@@ -18,8 +23,8 @@ func getPlayerOrder() -> Order:
 
 func setMarketValue(newVal: int) -> void:
 	marketVal = newVal
-	$MarketValueDisplay.text = str(marketVal)
-	$Graph.addPoint(newVal)
+	marketValueDisplay.text = str(marketVal)
+	graph.addPoint(newVal)
 	
 func _updateCashAndShares(nextOrder: Order) -> void:
 	match nextOrder.type:
@@ -31,30 +36,34 @@ func _updateCashAndShares(nextOrder: Order) -> void:
 			currentMoney -= (orderQuantity * marketVal)
 		Order.OrderType.HOLD:
 			pass
-	$PlayerCashDisplay.text = str(currentMoney) + " $UDS"
-	$PlayerSharesDisplay.text = str(currentShares) + " Shares"
+	playerCashDisplay.text = str(currentMoney) + " $UDS"
+	playerSharesDisplay.text = str(currentShares) + " Shares"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+	
+func _setOrderType(nextOrderType: Order.OrderType) -> void:
+	playerOrder = nextOrderType
+	orderVisualizer.updateOrder(nextOrderType)
+	
 func _on_sell_pressed() -> void:
 	print("Sell is pressed!")
 	if(orderQuantity < 1):
 		print("Stupid player, you can't sell less than 1 share!")
 	else:
-		playerOrder = Order.OrderType.SELL
+		_setOrderType(Order.OrderType.SELL)
 
 func _on_buy_pressed() -> void:
 	print("Buy is pressed!")
 	if(orderQuantity < 1):
 		print("Stupid player, you can't buy less than 1 share!")
 	else:
-		playerOrder = Order.OrderType.BUY
+		_setOrderType(Order.OrderType.BUY)
 
 func _on_hold_pressed() -> void:
 	print("Hold is pressed!")
-	playerOrder = Order.OrderType.HOLD
+	_setOrderType(Order.OrderType.HOLD)
 
 func _on_quantity_input_text_submitted(new_text: String) -> void:
 	print("Text changed! New line: " + new_text)
