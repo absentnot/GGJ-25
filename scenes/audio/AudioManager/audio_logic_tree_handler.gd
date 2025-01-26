6ff5fa584dee5ce01ebbaeb9bd8a18968032c71e
Zane Fadul <theboonandthezane@gmail.com>
2025-01-26 02:15:07 -0500

    fix

---
+++ scenes/audio/AudioManager/audio_logic_tree_handler.gd
@@ -0,0 +1,102 @@
+extends Node2D
+
+signal state_changed(action)
+signal handle_error(err_message)
+
+#=====State
+@onready var state_directory : set = set_state_directory
+func set_state_directory(sd): state_directory = sd
+
+var state = {}
+var prev_state = {}
+var will_update = false
+
+#=====Actions
+@onready var audio_actions : get = get_audio_actions, set = set_audio_actions
+func set_audio_actions(aa): audio_actions = aa
+func get_audio_actions(): return audio_actions
+
+#=====Tree
+@onready var trees : set = set_trees
+func set_trees(t): trees = t
+
+#======Example 
+#Below Example is akin to the following tree
+#
+#          _Mode_
+#        /     |                            \
+#       base   |fire -> fire_audio|          |ice -> ice_audio|
+#     
+#	where base:
+#
+#     low_health?
+#    /							\
+#	|true -> low_health_audio|   |false -> base_regular_audio|
+#
+
+#var example = {
+#	'mode' : {
+#		'base_mode':{
+#			'low_health':
+#				{'true':'low_health_audio',
+#				'false':'base'}}, 
+#		'fire_mode':'fire',
+#		'ice_mode':'ice'}
+#}
+
+
+func _ready():
+	connect('state_changed', Callable(get_parent(), '_on_state_changed'))
+	connect('handle_error', Callable(get_parent(), '_on_error'))
+	state = get_state()
+	
+func get_state():
+	var _state = {}
+	for variable in state_directory:
+		_state[variable] = str(state_directory[variable].call())
+	return _state
+
+func _compare_dict(dict1, dict2):
+	for key in dict1:
+		if dict2.has(key):
+			if dict1[key] != dict2[key]:
+				return false
+		else:
+			return false
+	return true
+	
+func _process(delta):
+	if not _compare_dict(state,prev_state):
+		if traverse(trees['root'],state) != traverse(trees['root'], prev_state):
+			will_update = true
+	if will_update:
+		emit_signal('state_changed', audio_actions[traverse(trees['root'], state)])
+		will_update = false
+	prev_state = state
+	state = get_state()
+	
+func traverse(tree, state):
+	var curr_node = null
+	var curr_path = null
+	for root in tree:
+		curr_node = root
+	curr_path = tree[curr_node]
+	while typeof(curr_path) != TYPE_STRING:
+		var curr_direction = ''
+		if state.has(curr_node):
+			curr_direction = str(state[curr_node])
+		else:
+			emit_signal("handle_error", curr_node + " not in the current tree path!")
+			return
+		if curr_path.has(curr_direction):
+			curr_path = curr_path[curr_direction]
+		else:
+			emit_signal('handle_error', curr_direction + ' not in the current tree path!')
+			return
+		if typeof(curr_path) == TYPE_STRING:
+			break
+		for root in curr_path: #subtree
+			curr_node = root
+		curr_path = curr_path[curr_node]
+	return curr_path
+	
---
+++ scenes/audio/AudioManager/audio_manager.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+var MY_NAME = "Magic Cassette"
+
+var audio_logic_tree_scene
+var song_controller_scene
+var audio_manager_config_scene
+
+func _ready():
+	print("%s is starting!" % MY_NAME)
+	var base_path = get_script().resource_path
+	var trimmed_path = base_path.substr(0, len(base_path)-16)
+	var audio_manager_config_scene = load(trimmed_path + 'audio_manager_config.tscn')
+	var config = audio_manager_config_scene.instantiate()
+	add_child(config)
+
+func _on_configured():
+	var config = $audio_manager_config
+	var base_path = get_script().resource_path
+	var trimmed_path = base_path.substr(0, len(base_path)-16)
+	var audio_logic_tree_scene = load(trimmed_path + 'audio_logic_tree_handler.tscn')
+	var song_controller_scene = load(trimmed_path + 'song_controller.tscn')
+	var audio_logic_tree_handler = audio_logic_tree_scene.instantiate()
+	var song_controller = song_controller_scene.instantiate()
+	audio_logic_tree_handler.set_state_directory(config.state_directory)
+	audio_logic_tree_handler.set_audio_actions(config.audio_actions)
+	audio_logic_tree_handler.set_trees(config.trees)
+	song_controller.set_tracks(config.tracks)
+	add_child(audio_logic_tree_handler)
+	add_child(song_controller)
+	
+func handle_actions(actionArray):
+	var audio_actions = $audio_logic_tree_handler.get_audio_actions()
+	for action in actionArray:
+		print('handle_actions ', action)
+		var action_to_perform = null
+		if typeof(action) == TYPE_DICTIONARY: action_to_perform = action
+		elif audio_actions.has(action): action_to_perform = audio_actions[action]
+		$song_controller.handle_action(action_to_perform)
+		
+func _on_perform_audio_action(action):
+	$song_controller.handle_action(action)
+	
+func _on_state_changed(action):
+	$song_controller.handle_action(action)
+
+func _on_error(err_message):
+	print('AudioManager encountered an error: ' + err_message)
+	$song_controller.clear()
+	set_process(false)
