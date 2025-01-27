72c6c2f0761ea3df77972b4642cf6ab4d898ffb2
NotLiu <aliu@lediur.com>
2025-01-26 19:46:23 -0600

    add logo

--- scenes/game_over.gd
+++ scenes/game_over.gd
@@ -3,9 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if !Global.gameWon:
-		$CenterContainer/VBoxContainer/RichTextLabel.text = "[wave amp=50.0 freq=2.0 connected=1][center]TOO BROKE TO CONTINUE 🦀[/center][/wave]"
-
+	#if !Global.gameWon:
+		#$CenterContainer/VBoxContainer/RichTextLabel.text = "[wave amp=50.0 freq=2.0 connected=1][center]TOO BROKE TO CONTINUE 🦀[/center][/wave]"
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
